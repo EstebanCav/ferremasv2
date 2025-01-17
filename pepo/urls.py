a15220caef3dbc0016d5f68873cf17552a67813e
Esteban <Este.cavieres@duocuc.cl>
2025-01-17 01:00:57 -0300

    ojala

--- pepo/urls.py
+++ pepo/urls.py
@@ -2,16 +2,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import HttpResponse  # Agregar esta importación
 
-def favicon_view(request):
-    return HttpResponse(status=204)
+
 
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('favicon.ico', favicon_view),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
