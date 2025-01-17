31005e4ede373fa2535cac315233250ff5106672
Esteban <Este.cavieres@duocuc.cl>
2025-01-17 02:54:25 -0300

    por favor

--- pepo/wsgi.py
+++ pepo/wsgi.py
@@ -15,5 +15,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pepo.settings')
 
 
-application = get_wsgi_application()
+app = get_wsgi_application()
 
