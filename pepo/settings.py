f01aacd56890e75ae5dee666211adfc20f550633
Esteban <Este.cavieres@duocuc.cl>
2025-01-17 02:47:20 -0300

    milagro

--- pepo/settings.py
+++ pepo/settings.py
@@ -13,11 +13,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path,os
 import os
-from dotenv import load_dotenv
-import dj_database_url
+import environ
+from pathlib import Path
 
-load_dotenv()
+env = environ.Env()
 
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,12 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6-^%dxw1&z3&u-l%y(3@@_7yw*67njo-uy+rmjzw=i7o4z3rag'
+SECRET_KEY =("django-insecure-6-^%dxw1&z3&u-l%y(3@@_7yw*67njo-uy+rmjzw=i7o4z3rag")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '.vercel.app']
 
 
 #'admin_interface',
@@ -91,13 +92,23 @@ WSGI_APPLICATION = 'pepo.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
+
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,  # Conexiones persistentes
-        ssl_require=True   # Forzar conexiones seguras
-    )
+    'default': {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": 'Y5Ur7De9kl1',  # Usando la variable de entorno
+        "HOST": 'db.bvmnvvqvxuynyabzomnp.supabase.co',          # Usando la variable de entorno
+        "PORT": "5432",
+        "OPTIONS": {
+            "sslmode": "verify-full",
+            "sslrootcert": os.path.join(BASE_DIR, 'prod-ca-2021.crt'),
+        }
+    }
 }
+
 # Configuración para PyMySQL (si usas MySQL/MariaDB)
 import pymysql
 pymysql.install_as_MySQLdb()
