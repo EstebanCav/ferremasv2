b62053980f92587fe84e69bbb2caa905c7aabf01
Esteban <Este.cavieres@duocuc.cl>
2025-01-22 01:17:23 -0300

    ayuda

--- pepo/settings.py
+++ pepo/settings.py
@@ -11,11 +11,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import environ
 from pathlib import Path,os
-from decouple import Config, Csv
+from pathlib import Path
 
-config = Config(".env")
+env = environ.Env()
 
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6-^%dxw1&z3&u-l%y(3@@_7yw*67njo-uy+rmjzw=i7o4z3rag'
+SECRET_KEY =("django-insecure-6-^%dxw1&z3&u-l%y(3@@_7yw*67njo-uy+rmjzw=i7o4z3rag")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app',]
+ALLOWED_HOSTS = ['*', '.vercel.app']
 
 
 #'admin_interface',
@@ -93,15 +95,14 @@ WSGI_APPLICATION = 'pepo.wsgi.app'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres.phfwfncgypqrlplqyqaa',
-        'PASSWORD': 'Y5Ur7De9kl1',
-        'HOST': 'aws-0-us-west-1.pooler.supabase.com',
-        'PORT': '6543',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": 'Y5Ur7De9kl1',  # Usando la variable de entorno
+        "HOST": 'db.phfwfncgypqrlplqyqaa.supabase.co',          # Usando la variable de entorno
+        "PORT": "5432",
     }
 }
-
 
 # Configuración para PyMySQL (si usas MySQL/MariaDB)
 import pymysql
@@ -146,9 +147,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = 'static/'
+STATICFILES_DIRS =[BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
